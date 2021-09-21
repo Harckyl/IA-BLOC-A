2d894f0fe9136ccd4147f2d76afd6de4380ecebe
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2021-09-21 16:38:31 -0400

    enelver les commentaires inutiles

--- function_arbre_pas_fonctionnel.py
+++ function_arbre_pas_fonctionnel.py
@@ -89,28 +89,5 @@ def Create_arbre(Matrice, depart):
    
 
 
-# racine.insert_gauche('B')
-# racine.insert_droit('F')
-
-# b_node = racine.get_gauche()
-# b_node.insert_gauche('C')
-# b_node.insert_droit('D')
-
-# f_node = racine.get_droit()
-# f_node.insert_gauche('G')
-# f_node.insert_droit('H')
-
-# c_node = b_node.get_gauche()
-# c_node.insert_droit('E')
-
-# g_node = f_node.get_gauche()
-# g_node.insert_gauche('I')
-
-# h_node = f_node.get_droit()
-# h_node.insert_droit('J')
-
-
-
-
 
 
