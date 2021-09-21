dbce0d91c95e8a2c8da5571d64a54848fdd44987
Harckyl <lucien.morcrette@gmail.com>
2021-09-21 14:28:28 -0400

    testing function to see python environement

--- Cleaningagent.py
+++ Cleaningagent.py
@@ -1,1 +1,13 @@
 import threading
+
+class cleaningAgent:
+    test = 1
+    def __init__(self):
+        self.test = 2
+    
+    def testing(self, testingvariable):
+        self.test = self.test + testingvariable
+        print(self.test)
+
+cleaningagent = cleaningAgent()
+cleaningagent.testing(12)
