83cd94c32d5d072996cdcc77efd94498a762c0c2
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2021-09-29 13:13:27 -0400

    ajout du parent de chaque node

--- function_create_arbre.py
+++ function_create_arbre.py
@@ -5,36 +5,45 @@ class Arbre:
       self.enfant_droit = None
       self.enfant_haut = None
       self.enfant_bas = None
+      self.parent = None
 
-   def insert_gauche(self, valeur):
+   def insert_gauche(self, valeur, parent):
       if self.enfant_gauche == None:
          self.enfant_gauche = Arbre(valeur)
+         self.enfant_gauche.parent = Arbre(parent)
       else:
          new_node = Arbre(valeur)
+         new_node.parent = Arbre(parent)
          new_node.enfant_gauche = self.enfant_gauche
          self.enfant_gauche = new_node
 
-   def insert_droit(self, valeur):
+   def insert_droit(self, valeur, parent):
       if self.enfant_droit == None:
          self.enfant_droit = Arbre(valeur)
+         self.enfant_droit.parent = Arbre(parent)
       else:
          new_node = Arbre(valeur)
+         new_node.parent = Arbre(parent)
          new_node.enfant_droit = self.enfant_droit
          self.enfant_droit = new_node
 
-   def insert_haut(self, valeur):
+   def insert_haut(self, valeur, parent):
       if self.enfant_haut == None:
          self.enfant_haut = Arbre(valeur)
+         self.enfant_haut.parent = Arbre(parent)
       else:
          new_node = Arbre(valeur)
+         new_node.parent = Arbre(parent)
          new_node.enfant_haut = self.enfant_haut
          self.enfant_haut = new_node
 
-   def insert_bas(self, valeur):
+   def insert_bas(self, valeur, parent):
       if self.enfant_bas == None:
          self.enfant_bas = Arbre(valeur)
+         self.enfant_bas.parent = Arbre(parent)
       else:
          new_node = Arbre(valeur)
+         new_node.parent = Arbre(parent)
          new_node.enfant_bas = self.enfant_bas
          self.enfant_bas = new_node
 
@@ -53,28 +62,39 @@ class Arbre:
    def get_bas(self):
       return self.enfant_bas
 
+   def get_parent(self):
+      return self.parent
+
    def Create_arbre(self, profondeur):
     if profondeur > 0:
        #déplacement vers la droite
         if self.get_valeur() + 10 <= 44:
-           self.insert_droit(self.get_valeur() + 10)
+           self.insert_droit(self.get_valeur() + 10, self.get_valeur())
            new_node = self.get_droit()
            new_node.Create_arbre(profondeur - 1)
+        else:
+         self.insert_droit(None, self.get_valeur())
        #déplacement vers la gauche
         if self.get_valeur() - 10 >= 0:
-           self.insert_gauche(self.get_valeur() - 10)
+           self.insert_gauche(self.get_valeur() - 10, self.get_valeur())
            new_node = self.get_gauche()
            new_node.Create_arbre(profondeur - 1)
+        else:
+         self.insert_gauche(None, self.get_valeur())
        #déplacement vers la bas
         if (self.get_valeur() + 1) % 10 < 5:
-           self.insert_bas(self.get_valeur() + 1)
+           self.insert_bas(self.get_valeur() + 1, self.get_valeur())
            new_node = self.get_bas()
            new_node.Create_arbre(profondeur - 1)
+        else:
+         self.insert_bas(None, self.get_valeur())
        #déplacement vers la haut
         if self.get_valeur() % 10 > 0:
-           self.insert_haut(self.get_valeur() - 1)
+           self.insert_haut(self.get_valeur() - 1, self.get_valeur())
            new_node = self.get_haut()
            new_node.Create_arbre(profondeur - 1)
+        else:
+         self.insert_haut(None, self.get_valeur())
    
 
 
@@ -99,7 +119,7 @@ for y in range(Colonne):
    for x in range(Ligne):
        Mamatrice[x][y] = int(str(x) + str(y))
        print (Mamatrice[x][y], end = " ")
-
+   print(" ")
 
 
 
@@ -115,6 +135,6 @@ racine.Create_arbre(8)
 
 
 
-print(affiche(racine))
+print(racine.get_bas().get_bas().get_haut().get_parent().get_valeur())
 
 
